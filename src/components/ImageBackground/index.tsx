d5fa8a1ec26aba171f5dcd659abdcaa0eb8ff78e
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-05-22 19:53:58 -0300

    refactor: optimize image background component with GSAP animations and improved resize handling

--- src/components/ImageBackground/index.tsx
+++ src/components/ImageBackground/index.tsx
@@ -2,62 +2,79 @@ import React from "react";
 import styles from "./ImageBackground.module.scss";
 import { useCursor } from "../../contexts/CursorContext";
 import Image from "next/image";
+import gsap from "gsap";
+
 function ImageBackground() {
-    const [imagesLoaded, setImagesLoaded] = React.useState(0);
-    const images = [
-        "/images/general/20210803_031452-scaled.jpg",
-        "/images/general/27159639._SX540_.jpg",
-        "/images/general/4c7ff6256079957c2770ea922741815e.jpg",
-        "/images/general/7108634.png",
-        "/images/general/Ghost-in-the-shell_ButWhyTho.png",
-        "/images/general/alita-.png",
-        "/images/general/images.jpg",
-    ];
-    const { position } = useCursor();
-    const [dimensions, setDimensions] = React.useState({
-        width: 0,
-        height: 0,
-    });
+  const [imagesLoaded, setImagesLoaded] = React.useState(0);
+  const imageBackgroundRef = React.useRef<HTMLDivElement>(null);
 
-    React.useEffect(() => {
-        const resize = () => {
-            setDimensions({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
+  const images = [
+    "/images/general/20210803_031452-scaled.jpg",
+    "/images/general/27159639._SX540_.jpg",
+    "/images/general/4c7ff6256079957c2770ea922741815e.jpg",
+    "/images/general/7108634.png",
+    "/images/general/Ghost-in-the-shell_ButWhyTho.png",
+    "/images/general/alita-.png",
+    "/images/general/images.jpg",
+  ];
+  const { position } = useCursor();
+  const [dimensions, setDimensions] = React.useState({
+    width: 0,
+    height: 0,
+  });
 
-        resize();
-        window.addEventListener("resize", resize);
-        return () => window.removeEventListener("resize", resize);
-    }, []);
-
-    const backgroundImageStyles = {
-        left: `${-(position.x / dimensions.width - 0.5) * 30}px`,
-        top: `${-(position.y / dimensions.height - 0.5) * 30 + 100}px`,
-        transform: `rotateX(${(position.y / dimensions.height - 0.5) * -15
-            }deg) rotateY(${(position.x / dimensions.width - 0.5) * 15}deg)`,
+  React.useEffect(() => {
+    const resize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     };
 
-    return (
-        <div className={styles["img-bg-container"]} style={backgroundImageStyles}>
-            {images.map((image: any, index: any) => {
-                return (
-                    <Image
-                        alt={`image_${index + 1}`}
-                        src={image}
-                        className={`${styles[`image_${index + 1}`]} ${styles["single-image"]
-                            }`}
-                        width={200}
-                        height={200}
-                        key={index}
-                        onLoad={() => setImagesLoaded(imagesLoaded + 1)}
-                    />
-                );
-            })}
-        </div>
-    );
+    resize();
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  React.useEffect(() => {
+    if (!imageBackgroundRef.current || dimensions.width === 0 || dimensions.height === 0) return;
+
+    const left = -(position.x / dimensions.width - 0.5) * 30 - 40;
+    const top = -(position.y / dimensions.height - 0.5) * 30 + 120;
+    const rotateX = (position.y / dimensions.height - 0.5) * -10;
+    const rotateY = (position.x / dimensions.width - 0.5) * 7;
+
+    gsap.to(imageBackgroundRef.current, {
+      left,
+      top,
+      rotateX,
+      rotateY,
+      perspective: 800,
+      // transformPerspective: 800,
+      ease: "power2",
+      duration: 1,
+    });
+  }, [position, dimensions]);
+
+  return (
+    <div
+      className={styles["img-bg-container"]}
+      ref={imageBackgroundRef}
+      style={{ position: "absolute" }} // Ensure absolute positioning
+    >
+      {images.map((image: any, index: any) => (
+        <Image
+          alt={`image_${index + 1}`}
+          src={image}
+          className={`${styles[`image_${index + 1}`]} ${styles["single-image"]}`}
+          width={200}
+          height={200}
+          key={index}
+          onLoad={() => setImagesLoaded(imagesLoaded + 1)}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ImageBackground;
-
