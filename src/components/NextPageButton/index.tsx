2a345a30524dc37ec458aaac8fcd0e15d71196a4
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-05-28 13:22:03 -0300

    refactor: Wrap Link component in AnimatePosOpacity for improved animation effects

--- src/components/NextPageButton/index.tsx
+++ src/components/NextPageButton/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "./NextPageButton.module.css";
 import Link from "next/link";
 import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+import AnimatePosOpacity from "../../utils/AnimatePosOpacity";
 interface INextPageButton {
   link: string;
   text: string;
@@ -30,52 +31,61 @@ const NextPageButton = ({
 
   return (
     <div className={styles["next-page-button"]}>
-      <Link
-        href={link}
-        className={styles["button-container"]}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        style={buttonContainerStyle}
-        scroll={false}
+      <AnimatePosOpacity
+        from={{ y: "200px" }}
+        to={{ y: 0 }}
+        durationIn={0.8}
+        durationOut={0.6}
+        delay={2}
+        set={{ y: "200px" }}
       >
-        {type === "backward" && (
-          <GoArrowLeft
-            size={30}
-            color={
-              showBackground
-                ? hover
-                  ? "#0c0c0c"
-                  : "#bebebe"
-                : hover
-                  ? "white"
-                  : "#bebebe"
-            }
-            className={styles["left-arrow"]}
-            style={{
-              marginRight: !hover ? "20px" : "40px",
-            }}
-          />
-        )}
-        <p dangerouslySetInnerHTML={{ __html: text }} />
-        {type === "forward" && (
-          <GoArrowRight
-            size={30}
-            color={
-              showBackground
-                ? hover
-                  ? "#0c0c0c"
-                  : "#bebebe"
-                : hover
-                  ? "white"
-                  : "#bebebe"
-            }
-            className={styles["right-arrow"]}
-            style={{
-              marginLeft: !hover ? "20px" : "40px",
-            }}
-          />
-        )}
-      </Link>
+        <Link
+          href={link}
+          className={styles["button-container"]}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          style={buttonContainerStyle}
+          scroll={false}
+        >
+          {type === "backward" && (
+            <GoArrowLeft
+              size={30}
+              color={
+                showBackground
+                  ? hover
+                    ? "#0c0c0c"
+                    : "#bebebe"
+                  : hover
+                    ? "white"
+                    : "#bebebe"
+              }
+              className={styles["left-arrow"]}
+              style={{
+                marginRight: !hover ? "20px" : "40px",
+              }}
+            />
+          )}
+          <p dangerouslySetInnerHTML={{ __html: text }} />
+          {type === "forward" && (
+            <GoArrowRight
+              size={30}
+              color={
+                showBackground
+                  ? hover
+                    ? "#0c0c0c"
+                    : "#bebebe"
+                  : hover
+                    ? "white"
+                    : "#bebebe"
+              }
+              className={styles["right-arrow"]}
+              style={{
+                marginLeft: !hover ? "20px" : "40px",
+              }}
+            />
+          )}
+        </Link>
+      </AnimatePosOpacity>
     </div>
   );
 };
