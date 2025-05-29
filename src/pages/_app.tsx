f7e3885f81781af72d984dbcb768d1c082ebb5d1
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-05-29 15:36:42 -0300

    feat: refactor Home page components and improve animation handling; streamline language and social media sections

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,42 +1,58 @@
-import React from "react";
 import type { AppProps } from "next/app";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
-import { DeviceContextProvider } from "../contexts/DeviceContext";
-import { PageContextProvider } from "../contexts/PageContext";
-import { LanguageProvider } from "../contexts/LanguageContext";
-import { CursorProvider } from "../contexts/CursorContext";
-import { TransitionProvider } from "../Layouts/TransitionProvider";
-import TransitionLayout from "../Layouts/TransitionLayout";
+import dynamic from "next/dynamic";
 import CursorFollower from "../components/CursorFollower";
-import Header from "../components/Header";
 import WebsiteHead from "../components/Head";
+import Header from "../components/Header";
 import Loading from "../components/Loading";
+import { CursorProvider } from "../contexts/CursorContext";
+import { DeviceContextProvider } from "../contexts/DeviceContext";
+import { LanguageProvider } from "../contexts/LanguageContext";
+import { PageContextProvider } from "../contexts/PageContext";
+import TransitionLayout from "../Layouts/TransitionLayout";
+import { TransitionProvider } from "../Layouts/TransitionProvider";
 
 import "../styles/scss/globals.css";
 
+const SpeedInsights = dynamic(
+  () => import("@vercel/speed-insights/next").then((mod) => mod.SpeedInsights),
+  {
+    ssr: process.env.NODE_ENV === "production",
+  }
+);
+
+/**
+ * The MyApp function serves as the custom App component for the Next.js application.
+ * It wraps the entire application with necessary providers and layouts, ensuring
+ * global state management, transitions, and shared components like the header and cursor follower.
+ */
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <SpeedInsights />
       <WebsiteHead />
-      <CursorProvider>
-        <TransitionProvider>
-          <TransitionLayout>
-            <PageContextProvider>
-              <DeviceContextProvider>
-                <LanguageProvider>
-                  <Loading pageRoute={router.route} />
-                  <CursorFollower />
-                  <Header />
-                  <Component {...pageProps} />
-                </LanguageProvider>
-              </DeviceContextProvider>
-            </PageContextProvider>
-          </TransitionLayout>
-        </TransitionProvider>
-      </CursorProvider>
+      <AppProviders>
+        <Loading pageRoute={router.route || "/"} />
+        <CursorFollower />
+        <Header />
+        <Component {...pageProps} />
+      </AppProviders>
     </>
+  );
+}
+
+function AppProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <CursorProvider>
+      <TransitionProvider>
+        <TransitionLayout>
+          <PageContextProvider>
+            <DeviceContextProvider>
+              <LanguageProvider>{children}</LanguageProvider>
+            </DeviceContextProvider>
+          </PageContextProvider>
+        </TransitionLayout>
+      </TransitionProvider>
+    </CursorProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,175 +1,178 @@
 import React from "react";
-import { FaLinkedin, FaInstagram, FaXTwitter } from "react-icons/fa6";
-import { FaMedium } from "react-icons/fa";
-import type { NextPage } from "next";
 import gsap from "gsap";
 import { SplitText } from "gsap/dist/SplitText";
-import { PageContext } from "../contexts/PageContext";
-
-gsap.registerPlugin(SplitText);
-
-import { useLanguage } from "../contexts/LanguageContext";
+import type { NextPage } from "next";
+import { FaMedium } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaXTwitter } from "react-icons/fa6";
+import { useIsomorphicLayoutEffect } from "usehooks-ts";
+import type { TransitionContextType } from "../Layouts/TransitionProvider";
+import { TransitionContext } from "../Layouts/TransitionProvider";
 import ImageBackground from "../components/ImageBackground";
+import NextPageButton from "../components/NextPageButton";
 import NoiseFilter from "../components/NoiseFilter";
 import { useCursor } from "../contexts/CursorContext";
-import styles from "./Home.module.css";
-import { useIsomorphicLayoutEffect } from "usehooks-ts";
-import { TransitionContext } from "../Layouts/TransitionProvider";
-import type { TransitionContextType } from "../Layouts/TransitionProvider";
-import AnimateInOut from "../utils/AnimateInOut";
-const textsList = ["Web Developer", "Art Enthusiast", "Creative Thinker"];
-import NextPageButton from "../components/NextPageButton";
+import { useLanguage } from "../contexts/LanguageContext";
+import { PageContext } from "../contexts/PageContext";
 import useDevice from "../hooks/useDevice";
+
+import styles from "./Home.module.css";
+
+gsap.registerPlugin(SplitText);
+const TEXTS_LIST = ["Web Developer", "Art Enthusiast", "Creative Thinker"];
+
+const SocialMediaLinks = () => {
+  const socialMediaLinksArr = [
+    {
+      url: "https://www.linkedin.com/in/lucas-oliveira-0b1a1b1b8/",
+      icon: <FaLinkedin />,
+    },
+    {
+      url: "https://www.instagram.com/lucas_oliveira.dev/",
+      icon: <FaInstagram />,
+    },
+    { url: "https://twitter.com/lucas_oliveira_", icon: <FaXTwitter /> },
+    { url: "https://medium.com/@lucas_oliveira", icon: <FaMedium /> },
+  ];
+
+  return (
+    <div className={styles["social-media-icons"]}>
+      {socialMediaLinksArr.map((social, index) => (
+        <a
+          key={index}
+          href={social.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {social.icon}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+type LanguageCode = "en" | "pt";
+
+type LanguageSelectorProps = {
+  language: LanguageCode;
+  changeLanguage: (lang: LanguageCode) => void;
+  handleModalPropsEnter: (text: string, scramble: boolean) => void;
+  handleModalPropsLeave: (text: string) => void;
+};
+
+const LanguageSelector = ({
+  changeLanguage,
+  handleModalPropsEnter,
+  handleModalPropsLeave,
+  language,
+}: LanguageSelectorProps) => (
+  <div className={styles["language-selector-container"]}>
+    {[
+      { code: "en", label: "EN", tooltip: "Switch language to English" },
+      { code: "pt", label: "PT", tooltip: "Switch language to Portuguese" },
+    ].map((lang, index, arr) => (
+      <React.Fragment key={lang.code}>
+        <button
+          className={`${styles["language-selector"]} ${language === lang.code ? styles.selected : ""}`}
+          onClick={() => changeLanguage(lang.code as "en" | "pt")}
+          onMouseEnter={() => handleModalPropsEnter(lang.tooltip, false)}
+          onMouseLeave={() => handleModalPropsLeave(lang.tooltip)}
+        >
+          {lang.label}
+        </button>
+        {index < arr.length - 1 && "/"}
+      </React.Fragment>
+    ))}
+  </div>
+);
 
 const Home: NextPage = () => {
   const { isMobile, isSmallTablet } = useDevice();
   const { timeline } = React.useContext(
     TransitionContext,
-  ) as TransitionContextType; // TransitionContextType
+  ) as TransitionContextType;
   const { isLoaded } = React.useContext(PageContext) as any;
-  // const [showElements, setShowElements] = React.useState<boolean>(false);
   const { language, changeLanguage } = useLanguage();
   const { handleModalPropsEnter, handleModalPropsLeave } = useCursor();
   const [titleIsReadyToAnimate, setTitleIsReadyToAnimate] =
     React.useState<boolean>(false);
-  // const [currentTitleText, setCurrentTitleText] = React.useState<number>(0);
-  // const [nextTitleText, setNextTitleText] = React.useState<number>(1);
   const [currentLocalTime, setCurrentLocalTime] = React.useState<string>("");
 
   const titleContainerRef = React.useRef<HTMLHeadingElement>(null);
 
+  // Setup exit animations
   useIsomorphicLayoutEffect(() => {
-    timeline.add(
-      gsap.to(`.${styles["language-selector-container"]}`, {
-        opacity: 0,
-        y: 50,
-        duration: 0.5,
-        ease: "power2.inOut",
-      }),
-      0,
-    );
+    const ctx = gsap.context(() => {
+      const animateOut = (selector: string) => {
+        return gsap.timeline().to(`.${styles[selector]}`, {
+          opacity: 0,
+          y: selector.includes("social") ? 50 : -50,
+          duration: 0.5,
+          ease: "power2.inOut",
+        });
+      };
 
-    timeline.add(
-      gsap.to(`.${styles["social-media-icons"]}`, {
-        opacity: 0,
-        y: 50,
-        duration: 0.5,
-        ease: "power2.inOut",
-      }),
-      0,
-    );
-    timeline.add(
-      gsap.to(`.${styles["welcome-message"]}`, {
-        opacity: 0,
-        y: -50,
-        duration: 0.5,
-        ease: "power2.inOut",
-      }),
-      0,
-    );
-    timeline.add(
-      gsap.to(`.${styles["title-container"]}`, {
-        opacity: 0,
-        y: -50,
-        duration: 0.5,
-        ease: "power2.inOut",
-      }),
-      0,
-    );
-    timeline.add(
-      gsap.to(`.${styles["current-position-text"]}`, {
-        opacity: 0,
-        y: -50,
-        duration: 0.5,
-        ease: "power2.inOut",
-      }),
-      0,
-    );
-    timeline.add(
-      gsap.to(`.${styles["location"]}`, {
-        opacity: 0,
-        y: -50,
-        duration: 0.5,
-        ease: "power2.inOut",
-      }),
-      0,
-    );
-  }, []);
+      // Add all animations to main timeline
+      [
+        "language-selector-container",
+        "social-media-icons",
+        "welcome-message",
+        "title-container",
+        "current-position-text",
+        "location",
+      ].forEach((selector) => {
+        timeline.add(animateOut(selector), 0);
+      });
+    });
 
+    return () => ctx.revert();
+  }, [timeline]);
+
+  // Handle entrance animations when page is loaded
   React.useEffect(() => {
-    if (isLoaded) {
-      setTitleIsReadyToAnimate(true);
-      setTimeout(() => {
-        gsap.to(`.${styles["language-selector-container"]}`, {
-          opacity: 1,
-          y: 0,
-          duration: 1,
-          ease: "power2.out",
-        });
-      }, 1000);
+    if (!isLoaded) return;
 
-      setTimeout(() => {
-        gsap.to(`.${styles["social-media-icons"]}`, {
-          opacity: 1,
-          y: 0,
-          duration: 1,
-          ease: "power2.out",
-        });
-      }, 1000);
+    setTitleIsReadyToAnimate(true);
 
-      setTimeout(() => {
-        gsap.to(`.${styles["welcome-message"]}`, {
+    setTimeout(() => {
+      const animateIn = (selector: string) => {
+        gsap.to(`.${styles[selector]}`, {
           opacity: 1,
           y: 0,
           duration: 1,
           ease: "power2.out",
         });
-      }, 1000);
+      };
 
-      setTimeout(() => {
-        gsap.to(`.${styles["title-container"]}`, {
-          opacity: 1,
-          y: 0,
-          duration: 1,
-          ease: "power2.out",
-        });
-      }, 1000);
-
-      setTimeout(() => {
-        gsap.to(`.${styles["current-position-text"]}`, {
-          opacity: 1,
-          y: 0,
-          duration: 1,
-          ease: "power2.out",
-        });
-      }, 1000);
-
-      setTimeout(() => {
-        gsap.to(`.${styles["location"]}`, {
-          opacity: 1,
-          y: 0,
-          duration: 1,
-          ease: "power2.out",
-        });
-      }, 1000);
-    }
+      [
+        "language-selector-container",
+        "social-media-icons",
+        "welcome-message",
+        "title-container",
+        "current-position-text",
+        "location",
+      ].forEach(animateIn);
+    }, 1000);
   }, [isLoaded]);
 
+  // Update local time
   React.useEffect(() => {
-    const interval = setInterval(() => {
-      const localTime = new Date().toLocaleString("pt-BR", {
+    const updateLocalTime = () => {
+      const localTime = new Intl.DateTimeFormat("pt-BR", {
         timeZone: "America/Sao_Paulo",
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
         hour12: true,
-      });
+      }).format(new Date());
       setCurrentLocalTime(localTime);
-    }, 1000);
+    };
 
+    updateLocalTime(); // Initialize immediately
+    const interval = setInterval(updateLocalTime, 1000);
     return () => clearInterval(interval);
   }, []);
 
+  // Handle title text animation
   React.useEffect(() => {
     if (!titleIsReadyToAnimate || !titleContainerRef.current) return;
 
@@ -177,11 +180,7 @@ const Home: NextPage = () => {
       const titles = gsap.utils.toArray<HTMLElement>(
         `.${styles["title-text"]}`,
       );
-
-      const tl = gsap.timeline({
-        repeat: -1,
-        delay: 1,
-      });
+      const tl = gsap.timeline({ repeat: -1, delay: 1 });
 
       titles.forEach((title) => {
         const splitText = new SplitText(title, {
@@ -199,12 +198,10 @@ const Home: NextPage = () => {
             ease: "power2.out",
           },
           "<",
-        );
-        tl.to(
+        ).to(
           splitText.chars,
           {
             opacity: 0,
-            // duration: 0.5,
             y: -100,
             delay: 2,
             stagger: 0.04,
@@ -215,9 +212,7 @@ const Home: NextPage = () => {
       });
     }, titleContainerRef);
 
-    return () => {
-      ctx.revert();
-    };
+    return () => ctx.revert();
   }, [titleIsReadyToAnimate]);
 
   return (
@@ -225,26 +220,29 @@ const Home: NextPage = () => {
       <div className={styles["content-container"]}>
         <div className={styles["location-container"]}>
           <p className={styles["location"]}>
-            Based in São Paulo,{" "}
-            <span dangerouslySetInnerHTML={{ __html: currentLocalTime }} />
+            Based in São Paulo, <span>{currentLocalTime}</span>
           </p>
         </div>
+
         <div className={styles["welcome-message-container"]}>
           <h1 className={styles["welcome-message"]}>
             Hi, i'm <span>Lucas Oliveira</span>
           </h1>
         </div>
+
         <div className={styles["title-container"]} ref={titleContainerRef}>
-          <h2 className={`${styles["title-text"]} initial`}>
-            <span>{textsList[0]}</span>
-          </h2>
-          <h2 className={`${styles["title-text"]} next`}>
-            <span>{textsList[1]}</span>
-          </h2>
-          <h2 className={`${styles["title-text"]} previous`}>
-            <span>{textsList[2]}</span>
-          </h2>
+          {TEXTS_LIST.map((text, index) => (
+            <h2
+              key={text}
+              className={`${styles["title-text"]} ${
+                index === 0 ? "initial" : index === 1 ? "next" : "previous"
+              }`}
+            >
+              <span>{text}</span>
+            </h2>
+          ))}
         </div>
+
         <div className={styles["current-position-container"]}>
           <p className={styles["current-position-text"]}>
             Currently <span>PL/SSR II Front End Developer</span> @{" "}
@@ -252,79 +250,28 @@ const Home: NextPage = () => {
           </p>
         </div>
       </div>
+
       <div className={styles["footer-row-container"]}>
         <div className={styles["footer-content"]}>
-          <div className={styles["social-media-icons"]}>
-            <a
-              href="https://www.linkedin.com/in/lucas-oliveira-0b1a1b1b8/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://www.instagram.com/lucas_oliveira.dev/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://twitter.com/lucas_oliveira_"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaXTwitter />
-            </a>
-            <a
-              href="https://medium.com/@lucas_oliveira"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaMedium />
-            </a>
-          </div>
-          <div className={styles["language-selector-container"]}>
-            <button
-              className={`${styles["language-selector"]} ${language === "en" ? styles.selected : ""}`}
-              onClick={() => {
-                changeLanguage("en");
-              }}
-              onMouseEnter={() => {
-                handleModalPropsEnter("Switch language to English", false);
-              }}
-              onMouseLeave={() => {
-                handleModalPropsLeave("Switch language to English");
-              }}
-            >
-              EN
-            </button>
-            /
-            <button
-              className={`${styles["language-selector"]} ${language === "pt" ? styles.selected : ""}`}
-              onClick={() => {
-                changeLanguage("pt");
-              }}
-              onMouseEnter={() => {
-                handleModalPropsEnter("Switch language to Portuguese", false);
-              }}
-              onMouseLeave={() => {
-                handleModalPropsLeave("Switch language to Portuguese");
-              }}
-            >
-              PT
-            </button>
-          </div>
+          <SocialMediaLinks />
+          <LanguageSelector
+            language={language}
+            changeLanguage={changeLanguage}
+            handleModalPropsEnter={handleModalPropsEnter}
+            handleModalPropsLeave={handleModalPropsLeave}
+          />
         </div>
       </div>
-      {isMobile || isSmallTablet ? (
+
+      {(isMobile || isSmallTablet) && (
         <NextPageButton
           link="/works"
           text="See my projects"
           showBackground={false}
           type="forward"
         />
-      ) : null}
+      )}
+
       <ImageBackground />
       <NoiseFilter />
     </section>
