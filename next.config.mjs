f5307fdeca3289aaf7c25dada3e2f583f403d121
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-11-20 11:52:14 -0300

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
   sassOptions: {
     implementation: 'sass-embedded',
   },
-  compilerOptions: {
-    baseUrl: './',
-    paths: {
-      '@/*': ['./src/*'],
-    },
-  }
 };
 
 export default nextConfig;
