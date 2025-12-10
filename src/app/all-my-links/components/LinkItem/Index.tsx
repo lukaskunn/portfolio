af4aed644e947df80e53ea5271656a5640269815
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-12-09 23:19:33 -0300

    feat: add LinkItem component and AllMyLinks page with styles for improved link presentation

---
+++ src/app/all-my-links/components/LinkItem/Index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "@/styles/css/all-my-links.module.css";
+
+type LinkItemProps = {
+  title: string;
+  link: string;
+  textHover: string;
+  openInNewPage?: boolean;
+};
+
+const LinkItem = ({ link, title, openInNewPage, textHover }: LinkItemProps) => {
+  return (
+    <a
+      href={link}
+      className={styles["single-item"]}
+      target={openInNewPage ? "_blank" : "_self"}
+      rel="noopener noreferrer"
+    >
+      <span
+        className={styles["item-hover"]}
+        dangerouslySetInnerHTML={{ __html: textHover }}
+      />
+      <span
+        className={styles["item-visible"]}
+        dangerouslySetInnerHTML={{ __html: title }}
+      />
+    </a>
+  );
+};
+
+export default LinkItem;
