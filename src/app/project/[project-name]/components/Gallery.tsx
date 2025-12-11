50fe408799a52548c5d42c52bc6c6c805d66c548
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-12-11 12:25:22 -0300

    Revert "refactor: add MasonryContainer component for improved gallery layout; update project details in en.json and package.json"
    This reverts commit 2c9f5d532df7e19bbfca90863b95a853b66d207e.

--- src/app/project/[project-name]/components/Gallery.tsx
+++ src/app/project/[project-name]/components/Gallery.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import styles from "@/styles/css/project.module.css"
 import GalleryItem from './GalleryItem'
-import MasonryContainer from './Masonry'
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
 
 interface GalleryItemData {
   image: string
@@ -29,11 +29,13 @@ const Gallery = ({ items }: GalleryProps) => {
       <h2 className={styles["gallery-title"]}>Project Gallery</h2>
       <div className={styles["mansory-container"]}>
         {isMounted && (
-          <MasonryContainer>
-            {items.map((item, index) => (
-              <GalleryItem key={index} image={item.image} caption={item.caption} />
-            ))}
-          </MasonryContainer>
+          <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 600: 2, 900: 3 }} gutterBreakPoints={{ 1: "0px" }}>
+            <Masonry>
+              {items.map((item, index) => (
+                <GalleryItem key={index} image={item.image} caption={item.caption} />
+              ))}
+            </Masonry>
+          </ResponsiveMasonry>
         )}
       </div>
     </section>
