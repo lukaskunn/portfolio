08fe87c6e49a4ce87a38cb987ca4be6e52b12c5a
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-12-12 23:39:15 -0300

    refactor: add redirects configuration to next.js setup

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   sassOptions: {
     implementation: 'sass-embedded',
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
