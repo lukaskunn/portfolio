e6872b85d5075bd46bde6c507accdafcb9d978b0
Lucas Oliveira <lucassioliveira098@gmail.com>
2025-12-26 12:40:53 -0300

    feat: update ProjectItem to simplify link structure and improve code clarity

--- src/app/projects/ProjectItem.tsx
+++ src/app/projects/ProjectItem.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import styles from '@/styles/css/projects.module.css'
 import { FiArrowUpRight } from "react-icons/fi";
-import Link from 'next/link';
 import LinkHandler from '@/components/LinkHandler';
 
 type ProjectItemProps = {
   project: {
     title: string;
-    link: { current: string };
+    link: string;
     tech: string;
     subtitle: string;
     urlToProject?: string;
@@ -21,14 +20,14 @@ const ProjectItem = ({ project, index, updateModal, openOnExternalPage = false }
   const { title, link, tech, subtitle, urlToProject } = project;
 
   return (
-    <div key={link.current}
+    <div key={link}
       style={{opacity: 0}}
       className={styles["project-list-item"]}
       data-project-item
       onMouseEnter={() => updateModal(index, true)}
       onMouseLeave={() => updateModal(index, false)}>
       {index === 0 ? <div className={`${styles["border"]} ${styles["top"]}`} /> : null}
-      <LinkHandler href={openOnExternalPage && urlToProject ? urlToProject : `/project/${link.current}`} className={styles["project-item"]} goToExternalPage={openOnExternalPage}>
+      <LinkHandler href={openOnExternalPage && urlToProject ? urlToProject : `/project/${link}`} className={styles["project-item"]} goToExternalPage={openOnExternalPage}>
         <p className={styles["project-index"]}>{index + 1 < 10 ? `0${index + 1}` : index + 1}</p>
         <div className={styles["project-title-subtitle"]}>
           <h2 className={styles["project-title"]}>{title}</h2>
