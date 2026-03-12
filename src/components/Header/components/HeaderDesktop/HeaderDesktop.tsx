67395ec5e2a0749d216575a95557e0c40cefe1ef
Lucas Oliveira <lucassioliveira098@gmail.com>
2026-03-12 12:30:43 -0300

    feat: enhance header layout and styling, update project item structure, and improve global styles

--- src/components/Header/components/HeaderDesktop/HeaderDesktop.tsx
+++ src/components/Header/components/HeaderDesktop/HeaderDesktop.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from "react";
 import HeaderItem from "./HeaderItem";
-import DownloadResumeButton from "./DownloadResumeButton";
 import ContactMeButton from "./ContactMeButton";
 import LinkHandler from "@/components/LinkHandler";
 import LineRevealContainer from "@/components/animations/LineReveal";
@@ -17,18 +16,21 @@ const HeaderDesktop = ({ data }: HeaderDesktopProps) => {
         <LinkHandler href="/home" className={styles["name-logo"]}>{data.headerTitle}</LinkHandler>
       </LineRevealContainer>
       <div className={styles["header-desktop__header-menu"]}>
-        {data.menuItems?.map((item: Link, index: number) => (
-          <LineRevealContainer key={item.text} direction="down" duration={ANIMATION_TIME.header} delay={ANIMATION_DELAYS.header + (index + 1) * 0.1}>
-            <HeaderItem title={item.text} link={item.url} />
-          </LineRevealContainer>
-        ))}
         <LineRevealContainer direction="down" duration={ANIMATION_TIME.header} delay={ANIMATION_DELAYS.header + ((data.menuItems?.length || 0) + 1) * 0.1}>
-          <DownloadResumeButton data={data.resumeButton} />
+          <ContactMeButton data={data.resumeButton} />
         </LineRevealContainer>
 
         <LineRevealContainer direction="down" duration={ANIMATION_TIME.header} delay={ANIMATION_DELAYS.header + ((data.menuItems?.length || 0) + 2) * 0.1}>
           <ContactMeButton data={data.contactButton} />
         </LineRevealContainer>
+      </div>
+
+      <div className={styles["header-desktop__menu-items"]}>
+        {data.menuItems?.map((item: Link, index: number) => (
+          <LineRevealContainer key={item.text} direction="down" duration={ANIMATION_TIME.header} delay={ANIMATION_DELAYS.header + (index + 1) * 0.1}>
+            <HeaderItem title={item.text} link={item.url} />
+          </LineRevealContainer>
+        ))}
       </div>
     </div>
   );
