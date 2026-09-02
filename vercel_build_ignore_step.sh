339c73577435e9b6f98dff7809838a881e9e33ac
Lucas Oliveira <lucassioliveira098@gmail.com>
2026-09-02 11:23:56 -0300

    update vercel config to only deploy master and develop branches

--- vercel_build_ignore_step.sh
+++ vercel_build_ignore_step.sh
@@ -1,8 +1,12 @@
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
 if [[ "$VERCEL_GIT_COMMIT_REF" == "master" || "$VERCEL_GIT_COMMIT_REF" == "develop"  ]] ; then
   # Proceed with the build
+  echo "✅ - Build can proceed"
   exit 1;
 
 else
   # Don't build
+  echo "🛑 - Build cancelled"
   exit 0;
 fi
